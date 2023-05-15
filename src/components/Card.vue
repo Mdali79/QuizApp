0f61cf837715b78dd434a9919e2048880f79f8ca
incognito2 <mohamodali2446@gmail.com>
2023-05-15 09:30:03 +0600

    final commit

---
+++ src/components/Card.vue
@@ -0,0 +1,62 @@
+<script setup>
+
+
+ import {defineProps} from "vue"
+ import  {useRouter} from "vue-router"
+
+ const {myquiz}=defineProps(['myquiz'])
+
+ const router = useRouter()
+
+ const navigateToQuiz =() =>{
+
+
+    router.push(`/quiz/${myquiz.id}`)
+ }
+
+
+ 
+</script>
+
+<template>
+
+    <div class="card" @click="navigateToQuiz">
+       
+        <img :src="myquiz.img" alt="">
+        <div class="card-tex">
+            <h2>{{myquiz.name}}</h2>
+            <p>{{ myquiz.questions.length }}</p>
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+.card{
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
+
+}
+
+.card img{
+   width: 100%;
+   height: 190px;
+   margin: 0;
+}
+
+.card .card-text{
+
+  padding:0 5px ;
+
+}
+
+.card .card-text h2{
+  font-weight: bold;
+}
+
+</style>
